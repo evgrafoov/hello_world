d9645d34d127ddc958deeb5c83a9bddd9c1fb994
evgrafoov <43775888+evgrafoov@users.noreply.github.com>
2018-11-27 11:27:43 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by Павел Евграфов on 27/11/2018.
+//  Copyright © 2018 Павел Евграфов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
